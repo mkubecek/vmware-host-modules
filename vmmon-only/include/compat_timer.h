111554af22a6984be2043f706c6a2009ce088a72
Michal Kubecek <mkubecek@suse.cz>
2017-11-27 08:24:36 +0100

    vmmon: convert timers to use timer_setup()
    With version 4.15-rc1, conversion to new timer interface is finished and
    old interface using init_timer() is no longer available. Convert all users
    to new one.

---
+++ vmmon-only/include/compat_timer.h
@@ -0,0 +1,26 @@
+#ifndef __COMPAT_TIMER_H__
+#   define __COMPAT_TIMER_H__
+
+#include <linux/timer.h>
+
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0)) && !defined(timer_setup)
+
+typedef unsigned long compat_timer_arg_t;
+
+static inline void timer_setup(struct timer_list *timer,
+				      void (*func)(compat_timer_arg_t),
+				      unsigned int flags)
+{
+	init_timer(timer);
+	timer->function = func;
+	timer->data = 0;
+	timer->flags = flags;
+}
+
+#else /* new timer interface since 4.15 */
+
+typedef struct timer_list *compat_timer_arg_t;
+
+#endif /* new timer interface since 4.15 */
+
+#endif /* __COMPAT_TIMER_H__ */
