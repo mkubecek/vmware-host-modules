f21b3bb04ceed9158e6cada65cd92b24933ab782
Michal Kubecek <mkubecek@suse.cz>
2019-09-21 00:45:53 +0200

    vmmon: use standard definition of MSR_TSX_FORCE_ABORT if available
    The MSR_TSX_FORCE_ABORT macro is defined in mainline since commit
    52f64909409c ("x86: Add TSX Force Abort CPUID/MSR") in v5.1-rc1.

--- vmmon-only/include/x86msr.h
+++ vmmon-only/include/x86msr.h
@@ -633,7 +633,9 @@ typedef enum {
 #define MSR_HYPERV_GUESTOSID_OS_WINNT_DERIVATIVE 4ULL
 
 /* MSR for forcing RTM abort to recover PMC3 (see PR 2333817) */
+#ifndef MSR_TSX_FORCE_ABORT
 #define MSR_TSX_FORCE_ABORT                      0x0000010f
+#endif
 #define MSR_TSX_FORCE_ABORT_RTM_BIT_INDEX        0
 
 /*
