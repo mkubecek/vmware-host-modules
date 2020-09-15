d088fe7d0c4d16a5ae3bd0b0c5324984e8187cc2
Michal Kubecek <mkubecek@suse.cz>
2020-09-15 18:58:14 +0200

    vmmon: rename GDT_SIZE macro to avoid collision
    When building against kernel 4.12 and newer, macro name GDT_SIZE used in
    (vmmon) include/segs.h collides with macro defined in (kernel source)
    arch/x86/include/asm/segment.h, resulting in its redefinition.
    
    To prevent potential problems, rename vmmon's GDT_SIZE to VMMON_GDT_SIZE
    and GDT_LIMIT to VMMON_GDT_LIMIT. (There is no GDT_LIMIT in mainline kernel
    source but let's be consistent.)

--- vmmon-only/include/segs.h
+++ vmmon-only/include/segs.h
@@ -51,14 +51,14 @@
  * it can use a single cross GDT).
  */
 
-#define GDT_SIZE              (sizeof(Descriptor) * NUM_VALID_SEGMENTS)
-#define GDT_LIMIT             (GDT_SIZE - 1)
+#define VMMON_GDT_SIZE        (sizeof(Descriptor) * NUM_VALID_SEGMENTS)
+#define VMMON_GDT_LIMIT       (VMMON_GDT_SIZE - 1)
 
 #define IRB_SIZE              32 /* Interrupt redirection bitmap. */
 #define TSS_SIZE              (sizeof(Task64) + IRB_SIZE)
 
 #define GDT_START_VA          (VPN_2_VA(GDT_AND_TASK_START))
-#define TASK_START_VA         (VPN_2_VA(GDT_AND_TASK_START) + GDT_SIZE)
+#define TASK_START_VA         (VPN_2_VA(GDT_AND_TASK_START) + VMMON_GDT_SIZE)
 
 /*
  * vmkBoot uses some of the lower-numbered segments, as do host kernels on
