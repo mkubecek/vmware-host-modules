ae9ab4756eeba8cafd020a64c9febbe2c94a6ba7
Michal Kubecek <mkubecek@suse.cz>
2022-11-18 13:42:16 +0100

    modules: use standard constant definitions if available
    The PCI_VENDOR_ID_VMWARE macro is defined in mainline pci_ids.h since
    commit 94e57fea6202 ("PCI: Move PCI_VENDOR_ID_VMWARE to pci_ids.h") in
    v3.18-rc1.
    
    The PCI_DEVICE_ID_VMWARE_VMXNET3 macro is defined in mainline pci_ids.h
    since commit b1226c7db1d9 ("vmxnet3: Move PCI Id to pci_ids.h") in
    v4.10-rc1.
    
    The MSR_MISC_FEATURES_ENABLES_CPUID_FAULT macro is defined in mainline
    since commit e9ea1e7f53b8 ("x86/arch_prctl: Add ARCH_[GET|SET]_CPUID") in
    v4.12-rc1.
    
    The CR3_PCID_MASK is defined in mainline asm/processor-flags.h since commit
    6c690ee1039b ("x86/mm: Split read_cr3() into read_cr3_pa() and
    __read_cr3()") in v4.13-rc1.

--- vmnet-only/vm_device_version.h
+++ vmnet-only/vm_device_version.h
@@ -35,6 +35,8 @@
 #endif
 #endif
 
+#include <linux/pci_ids.h>
+
 /* LSILogic 53C1030 Parallel SCSI controller
  * LSILogic SAS1068 SAS controller
  */
@@ -54,7 +56,10 @@
  *    VMware HD Audio codec
  *    VMware HD Audio controller
  */
+#ifndef PCI_VENDOR_ID_VMWARE
 #define PCI_VENDOR_ID_VMWARE                    0x15AD
+#endif
+
 #define PCI_DEVICE_ID_VMWARE_SBX                0x0420
 #define PCI_DEVICE_ID_VMWARE_SVGA4              0x0408
 #define PCI_DEVICE_ID_VMWARE_SVGA_EFI           0x0407
@@ -76,7 +81,11 @@
 #define PCI_DEVICE_ID_VMWARE_1394               0x0780
 #define PCI_DEVICE_ID_VMWARE_BRIDGE             0x0790
 #define PCI_DEVICE_ID_VMWARE_ROOTPORT           0x07A0
+
+#ifndef PCI_DEVICE_ID_VMWARE_VMXNET3
 #define PCI_DEVICE_ID_VMWARE_VMXNET3            0x07B0
+#endif
+
 #define PCI_DEVICE_ID_VMWARE_PVSCSI             0x07C0
 #define PCI_DEVICE_ID_VMWARE_82574              0x07D0
 #define PCI_DEVICE_ID_VMWARE_AHCI               0x07E0
