13af167b09d44eb036bd8dbf2cb06064434dc6bd
Michal Kubecek <mkubecek@suse.cz>
2022-11-27 11:50:29 +0100

    modules: more hacks for old gcc
    While mainline kernel raised its requirements for gcc version already, much
    older versions are still in use in supported RHEL/SLES releases. Add two
    more hacks to fix build with e.g. gcc 4.8.
    
    The infamous ASSERT_ON_COMPILE_SELECTOR_SIZE macro was modified recently to
    use __builtin_choose_expr() with the rationale that it should work on any
    gcc >= 4.9 which includes all versions supported by current mainline
    kernel. Alas, older gcc is used in e.g. RHEL 7 or SLES 12 so we need to
    resurrect the old dirty hack for gcc < 5.
    
    The preprocessor evaluates the condition of #elif even if the first #if
    branch was taken so that we cannot use "#elif __has_attribute()" shortcut
    in the fallthrough pseudokeyword handling code. Use nested #if instead.

--- vmmon-only/include/vm_asm_x86.h
+++ vmmon-only/include/vm_asm_x86.h
@@ -65,11 +65,18 @@
  * https://gcc.gnu.org/bugzilla/show_bug.cgi?id=79482
  */
 #ifndef USE_UBSAN
+#if __GNUC__ >= 5
 #define ASSERT_ON_COMPILE_SELECTOR_SIZE(expr)                                \
    ASSERT_ON_COMPILE(sizeof(Selector) == 2 &&                                \
                      __builtin_choose_expr(__builtin_constant_p(expr),       \
                                            ((expr) >> 16) == 0,              \
                                            sizeof(expr) <= 2))
+#else /* __GNUC__ < 5 */
+#define ASSERT_ON_COMPILE_SELECTOR_SIZE(expr)                                \
+   ASSERT_ON_COMPILE(sizeof(Selector) == 2 &&                                \
+		     ((__builtin_constant_p(expr) && ((expr) >> 16) == 0) || \
+		      sizeof(expr) <= 2))
+#endif /* __GNUC__ >= 5 */
 #else
 #define ASSERT_ON_COMPILE_SELECTOR_SIZE(expr)
 #endif
