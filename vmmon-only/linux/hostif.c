aeb85e07982b3f3265ae4634bff542edbd179725
Michal Kubecek <mkubecek@suse.cz>
2024-01-12 08:30:33 +0100

    modules: fix build with -Wmissing-prototypes
    Mainline commit 0fcb70851fbf ("Makefile.extrawarn: turn on
    missing-prototypes globally") in 6.8-rc1 enables -Wmissing-prototypes
    globally, revealing a lot of unclean code and also some actual problems.
    This is also the case in vmmon and vmnet modules.
    
    Most of them are addressed by making functions used only within one file
    static. The missing prototype of random_get_entropy_fallback() is handled
    by including <linux/timex.h> rather than <asm/timex.h>.
    
    Finally, there are four functions in vmnet module which are actually used
    in multiple files but instead of proper declarations, their prototype is
    duplicated in vmnet-only/driver.c, risking that the two copies won't match
    (which actually happened in one case). The cleanest solution would be
    creating separate header files for them (bridge.h, netif.h, userif.h and
    vnetUserListener.h) and including them in the respective source file and
    driver.c. As the developers already handle similar cases by simply putting
    the declarations into vnetInt.h, let us do the same to keep things simple.

--- vmmon-only/linux/hostif.c
+++ vmmon-only/linux/hostif.c
@@ -2942,7 +2942,7 @@ HostIF_CallOnEachCPU(void (*func)(void*), // IN: function to call
  *-----------------------------------------------------------------------------
  */
 
-Bool
+static Bool
 HostIFCheckTrackedMPN(VMDriver *vm, // IN: The VM instance
                       MPN mpn)      // IN: The MPN
 {
@@ -3062,7 +3062,7 @@ HostIF_ReadPhysical(VMDriver *vm,      // IN: The VM instance
  *----------------------------------------------------------------------
  */
 
-int
+static int
 HostIFWritePhysicalWork(MA ma,             // MA to be written to
                         VA64 addr,         // src data to write
                         Bool kernelBuffer, // is the buffer in kernel space?
@@ -3221,7 +3221,7 @@ HostIF_GetCurrentPCPU(void)
  *----------------------------------------------------------------------
  */
 
-int
+static int
 HostIFStartTimer(Bool rateChanged,  //IN: Did rate change?
                  unsigned int rate) //IN: current clock rate
 {
